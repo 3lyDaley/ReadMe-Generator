3d0c3ec2dfcb9a0ebefabad1ddf51e941a521004
Bad Boys Club <3ly.daley@gmail.com>
2022-06-10 16:17:58 -0700

    added user prompts

--- starterCode/Develop/index.js
+++ starterCode/Develop/index.js
@@ -1,9 +1,40 @@
 // TODO: Include packages needed for this application
-console.log('hello index.js');
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
 const questions = [];
+const promptUser= () => {
+  return inquirer.prompt([
+    {
+      type: 'input',
+      name: 'github',
+      message: 'What is your Github username? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true;
+        } else { 
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+    {
+      type: 'input',
+      name: 'email',
+      message: 'What is your Email address? (Required)',
+      validate: emailInput => {
+        if (emailInput) {
+          return true;
+        } else { 
+          console.log('Please enter your name!');
+          return false;
+        }
+      }
+    },
+  ])
+}
 
+promptUser().then(console.log(answers));
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
